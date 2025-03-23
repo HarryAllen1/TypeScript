9d233651c28d37c74c9f416fd4280d11917e29ff
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2025-03-23 14:37:52 -0700

    Improve type signature for Math.sign

--- src/lib/es2015.core.d.ts
+++ src/lib/es2015.core.d.ts
@@ -97,7 +97,7 @@ interface Math {
      * Returns the sign of the x, indicating whether x is positive, negative or zero.
      * @param x The numeric expression to test
      */
-    sign(x: number): number;
+    sign(x: number): 0 | -1 | 1;
 
     /**
      * Returns the base 10 logarithm of a number.
